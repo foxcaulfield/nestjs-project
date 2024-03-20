d8db513a184dcdb6ac3d9e558b5e524f7bb543ec
foxcaulfield <fox.caulfield@gmail.com>
2024-03-20 22:39:46 +0100

    Update server response

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,7 +2,7 @@ import http from "http";
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "Content-type": "text/plain" });
-	res.end("Hi! " + req.socket.remoteAddress);
+	res.end("Hi! " + req.socket.remoteAddress + " " + req.headers["x-forwarded-for"]);
 });
 
 server.listen(3000);
