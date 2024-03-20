e4aea221a10d16e5b8e4572041b658c8f12d42dd
foxcaulfield <fox.caulfield@gmail.com>
2024-03-20 22:38:57 +0100

    Update server response

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,8 @@
 import http from "http";
 
-const server = http.createServer((_req, res) => {
+const server = http.createServer((req, res) => {
 	res.writeHead(200, { "Content-type": "text/plain" });
-	res.end("Hi!");
+	res.end("Hi! " + req.socket.remoteAddress);
 });
 
 server.listen(3000);
