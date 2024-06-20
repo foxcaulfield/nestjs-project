fca9c102638ee3b0e8562ccd70a54abac87248ea
foxcaulfield <fox.caulfield@gmail.com>
2024-06-20 20:17:18 +0200

    Implement prisma-orm.service

---
+++ src/prisma-orm/prisma-orm.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+
+@Injectable()
+export class PrismaOrmService
+	extends PrismaClient
+	implements OnModuleInit, OnModuleDestroy
+{
+	public async onModuleInit(): Promise<void> {
+		try {
+			return await this.$connect();
+		} catch (error) {
+			console.log("Error occured during connection to DB");
+		}
+	}
+
+	public async onModuleDestroy(): Promise<void> {
+		return await this.$disconnect();
+	}
+}
+
+//   async enableShutdownHooks(app: INestApplication) {
+//     this.$on<INestApplication>('beforExit', async () => {
+//       await app.close();
+//     });
+//   }
