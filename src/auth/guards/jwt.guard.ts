b894385c920caa49213a205c19c6a79a46c6b20d
foxcaulfield <fox.caulfield@gmail.com>
2024-06-22 14:22:46 +0200

    Update logging in guards

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -1,13 +1,17 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("jwt") {
+	private readonly logger: Logger = new Logger(JwtAuthGuard.name);
+
 	public canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		console.log("Inside JwtAuthGuard canActivate");
+		// const logMessage = `${context.getClass().name} ${context.getHandler().name}`;
+		const logMessage = "Guard";
+		this.logger.log(logMessage);
 		return super.canActivate(context);
 	}
 }
--- src/auth/guards/local.guard.ts
+++ src/auth/guards/local.guard.ts
@@ -1,13 +1,17 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
+import { ExecutionContext, Injectable, Logger } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
 @Injectable()
 export class LocalGuard extends AuthGuard("local") {
+	private readonly logger: Logger = new Logger(LocalGuard.name);
+
 	public canActivate(
 		context: ExecutionContext,
 	): boolean | Promise<boolean> | Observable<boolean> {
-		console.log("Inside LocalGuard canActivate");
+		// const logMessage = `${context.getClass().name} ${context.getHandler().name}`;
+		const logMessage = "Guard";
+		this.logger.log(logMessage);
 		return super.canActivate(context);
 	}
 }
