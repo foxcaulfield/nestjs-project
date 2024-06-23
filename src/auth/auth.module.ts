e91ef9fd9a772398c0be8993caaa2de8ce903300
Maxim R <79020992+foxcaulfield@users.noreply.github.com>
2024-06-23 18:18:32 +0200

    Update auth.module.ts
    NestJS modules are the basic building blocks of an application

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,25 +1,19 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
-// import { UsersModule } from "src/users/users.module";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
-import { UsersService } from "src/users/users.service";
 import { JwtStrategy } from "./strategies/jwt.strategy";
 import { LocalStrategy } from "./strategies/local.strategy";
 import { SystemModule } from "src/system/system.module";
 import { RolesGuard } from "./guards/roles.guard";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
 	controllers: [AuthController],
-	providers: [
-		AuthService,
-		UsersService,
-		LocalStrategy,
-		JwtStrategy,
-		RolesGuard,
-	],
+	providers: [AuthService, LocalStrategy, JwtStrategy, RolesGuard],
 	imports: [
+		UsersModule,
 		SystemModule,
 		PassportModule,
 		JwtModule.register({
