855c5f5648ff762b147406c04a75a31b3a1fd1ab
foxcaulfield <fox.caulfield@gmail.com>
2024-06-23 14:51:38 +0200

    Update sign-payload

--- src/auth/dto/sign-payload.dto.ts
+++ src/auth/dto/sign-payload.dto.ts
@@ -1,6 +1,7 @@
-import { User } from "@prisma/client";
+import { Role, User } from "@prisma/client";
 
 export class SignPayloadDto implements Partial<User> {
 	public id: number;
 	public username: string;
+	public role: Role;
 }
